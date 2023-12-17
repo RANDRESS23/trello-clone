65f5387c77db139792dd7b6a68799d9a104b317c
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-12-17 17:48:24 -0500

    Adding the sidebar and navbar in the dashboard page

---
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,25 @@
+// import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
+import { OrgControl } from "./_components/org-control";
+
+// export async function generateMetadata() {
+//   const { orgSlug } = auth();
+
+//   return {
+//     title: startCase(orgSlug || "organization"),
+//   };
+// };
+
+export default function OrganizationIdLayout ({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
