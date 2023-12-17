52491c2533adfa3bad97e57deb810a1486a2c540
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-12-16 21:13:08 -0500

    Adding the clerk service

---
+++ src/app/(platform)/layout.tsx
@@ -0,0 +1,21 @@
+// import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+
+// import { ModalProvider } from "@/components/providers/modal-provider";
+// import { QueryProvider } from "@/components/providers/query-provider";
+
+export default function PlatformLayout ({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      {/* <QueryProvider> */}
+        {/* <Toaster /> */}
+        {/* <ModalProvider /> */}
+        {children}
+      {/* </QueryProvider> */}
+    </ClerkProvider>
+  );
+};
